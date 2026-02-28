1f08311f6c943d2954d3432395e9aabdf4473cc9
carvejade <jiwonseo199@gmail.com>
2026-02-28 23:25:05 +0900

    [Bronze IV] Title: 치즈버거 만들기, Time: 100 ms, Memory: 14128 KB -BaekjoonHub

---
+++ 백준/Bronze/30017. 치즈버거 만들기/치즈버거 만들기.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int A = Integer.parseInt(st.nextToken());
+        int B = Integer.parseInt(st.nextToken());
+
+        int x = Math.min(B, A - 1);
+        System.out.println(2 * x + 1);
+    }
+}
