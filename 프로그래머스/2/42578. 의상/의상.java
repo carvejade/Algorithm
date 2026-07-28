3dec1c478be24bf705a619f8c8b7b31d1bf1756f
Jiwon Seo <jiwonseo199@gmail.com>
2026-07-28 11:53:19 +0900

    [level 2] Title: 의상, Time: 0.92 ms, Memory: 86 MB -BaekjoonHub

---
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -0,0 +1,18 @@
+import java.util.*;
+
+class Solution {
+    public int solution(String[][] clothes) {
+        int answer = 1;
+        HashMap<String,Integer>map = new HashMap <>();
+        
+        for(int i=0; i<clothes.length; i++){
+            map.put(clothes[i][1],map.getOrDefault(clothes[i][1],0)+1);
+        }
+        for(String k : map.keySet()){
+            //keySet : 키 전체 => entrySet
+            answer *=(map.get(k) +1);
+        }
+        answer--;
+        return answer;
+    }
+}
